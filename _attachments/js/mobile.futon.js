739effe289ad80404738314e85e71122bb8aa645
Dale Harvey <daleharvey@arandomurl.com>
2011-06-01 03:53:34 +0100

    first commit

---
+++ _attachments/js/mobile.futon.js
@@ -0,0 +1,217 @@
+window.log = function(){
+  log.history = log.history || [];
+  log.history.push(arguments);
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
+};
+
+$.ajaxSetup({
+  cache: false
+});
+
+// Doesnt handle ghosted events, will survive for now
+var pressed = Utils.isMobile() ? "click" : "click";
+
+var Tasks = (function () {
+
+  var mainDb  = document.location.pathname.split("/")[1],
+      paneWidth = 0,
+      isMobile = Utils.isMobile(),
+      router  = new Router(),
+      current_tpl = null,
+      slidePane = null,
+      docs    = {},
+      tasks   = [],
+      servers = [],
+      zIndex  = 0,
+      currentOffset = 0,
+      lastPane = null,
+      $db     = $.couch.db(mainDb);
+
+  var templates = {
+    home_tpl : {
+      transition : "slideHorizontal",
+      events : {
+      },
+      init : function(dom) {
+      }
+    }
+  };
+
+  router.get(/^(!)?$/, function () {
+    $("#title").text("CouchDB");          
+    render(/^(!)?$/, "home_tpl", null);
+  });
+
+  router.get("!/couchapps/", function () {
+    
+    // 
+    var completed = 0;
+    var couchapps = [];
+    
+    function designDocs(database) {
+      return $.couch.db(database).allDesignDocs();
+    }
+    
+    function isCouchApp(ddoc, max) { 
+        var url = "/" + ddoc.database + "/" + ddoc.ddoc + "/index.html";
+        $.ajax({
+            type:"HEAD", 
+            url:url, 
+            complete: function(xhr) {
+                completed++;
+                if (xhr.status === 200) { 
+                    couchapps.push({url:url, name:ddoc.ddoc.split("/")[1]});
+                }
+                if (completed === max) { 
+                    $("#title").text("Couchapps");        
+                    render("!/couchapps/", "couchapps_tpl", {couchapps:couchapps});
+                }
+            }
+        });
+    }
+    
+    $.couch.allDbs({
+      success: function(data) {
+        $.when.apply(this, $.map(data, designDocs)).then(function() {
+            var designDocs = [];
+            $.each(arguments, function(id, ddocs) { 
+                $.each(ddocs[0].rows, function(ddocid, ddoc) { 
+                    designDocs.push({database:data[id], ddoc:ddoc.id});
+                });
+            });
+            
+            $.map(designDocs, function(ddoc) { 
+                isCouchApp(ddoc, designDocs.length);
+            });
+            
+        });
+      }
+    });
+  });
+
+  router.get("!/databases/", function () {
+    $.couch.allDbs({
+      success: function(data) {
+        $("#title").text("Databases");
+        render("!/databases/", "databases_tpl", {databases:data});
+      }
+    });
+  });
+
+  router.get("!/replication/", function () {
+    $.couch.allDbs({
+      success: function(data) {
+        $("#title").text("Replication");
+        render("!/replication/", "replication_tpl", {databases:data});
+      }
+    });
+  });
+
+  function render(url, tpl, data) {
+
+   if (router.matchesCurrent(url) === null) {
+      return;
+    }
+
+    data = data || {};
+    $("body").removeClass(current_tpl).addClass(tpl);
+    
+    var rendered = Mustache.to_html($("#" + tpl).html(), data),
+    $pane = $("<div class='pane'><div class='content'>" + rendered + "</div></div>");
+
+    // Bind this templates events
+    var events = templates[tpl] && templates[tpl].events;
+    if (events) {
+      for (var key in events) {
+        $(key, $pane).bind(events[key].event + ".custom", events[key].callback);
+      }
+    }
+
+    if (templates[tpl] && templates[tpl].init) {
+      templates[tpl].init($pane);
+    }
+
+    var transition = templates[tpl] && templates[tpl].transition;
+
+    if (transition === 'slideUp') {
+
+      $("#content").one("webkitTransitionEnd transitionend", function() {
+        if (lastPane) {
+          lastPane.hide();
+        }
+      });
+
+      slidePane = $pane.addClass("slidepane")
+        .css({left:currentOffset, top:-$(window).height(), 'z-index': 3})
+        .appendTo("#content");
+      transformY(slidePane, $(window).height());
+
+    } else if (slidePane) {
+
+      if (lastPane) {
+        lastPane.remove();
+        lastPane = null;
+      }
+
+      $pane.css({"left":currentOffset}).appendTo($("#content"));
+      transformY(slidePane, 0);
+      lastPane = $pane;
+
+      slidePane.one("webkitTransitionEnd transitionend", function() {
+        slidePane.remove();
+        slidePane = null;
+      });
+
+    } else {
+
+      if (current_tpl) {
+        currentOffset += true ? paneWidth : -paneWidth;
+      }
+
+      var tmp = lastPane;
+      $("#content").one("webkitTransitionEnd transitionend", function() {
+        if (tmp) {
+          tmp.remove();
+          tmp = null;
+        }
+      });
+
+      transformX($pane, currentOffset);
+      $pane.appendTo($("#content"));
+
+      transformX($("#content"), -currentOffset);
+      lastPane = $pane;
+    }
+    current_tpl = tpl;
+  }
+
+  function transformY(dom, x) {
+    if (Modernizr.csstransforms3d) {
+      dom.css("-moz-transform", "translate3d(0, " + x + "px, 0)")
+        .css("-webkit-transform", "translate3d(0, " + x + "px, 0)");
+    } else {
+      dom.css("-moz-transform", "translate(0, " + x + "px)")
+        .css("-webkit-transform", "translate(0, " + x + "px)");
+    }
+  }
+
+  function transformX(dom, x) {
+    if (Modernizr.csstransforms3d) {
+      dom.css("-moz-transform", "translate3d(" + x + "px, 0, 0)")
+        .css("-webkit-transform", "translate3d(" + x + "px, 0, 0)");
+    } else {
+      dom.css("-moz-transform", "translate(" + x + "px, 0)")
+        .css("-webkit-transform", "translate(" + x + "px, 0)");
+    }
+  }
+
+  $(window).bind("resize", function () {
+    paneWidth = $("body").width();
+  });
+  $(window).resize();
+
+  router.init();
+
+})();
---
+++ _attachments/js/utils.js
@@ -0,0 +1,328 @@
+// Err a little NIH, some of the regex stuff is from sammy, but I only
+// wanted the routing, and the way sammy implemented routing isnt
+// great for my cpu (it polls by default even when the hashchange
+// is supported)
+var Router = (function() {
+
+  var PATH_REPLACER = "([^\/]+)",
+      PATH_MATCHER  = /:([\w\d]+)/g,
+      preRouterFun  = null,
+      fun404        = null,
+      history       = [],
+      routes        = {GET: [], POST: []};
+
+  // Needs namespaced and decoupled and stuff
+  function init() {
+    $(window).bind("hashchange", urlChanged).trigger("hashchange");
+    $(document).bind("submit", formSubmitted);
+  };
+
+  function back() {
+    history.pop(); // current url
+    if (history.length > 0) {
+      document.location.href = "#" + history.pop();
+    } else {
+      document.location.href = "#";
+    }
+  };
+
+  function get(path, cb) {
+    route("GET", path, cb);
+  };
+
+  function post(path, cb) {
+    route("POST", path, cb);
+  };
+
+  function refresh(maintainScroll) {
+    urlChanged(maintainScroll);
+  };
+
+  function preRouter(fun) {
+    preRouterFun = fun;
+  };
+
+  function error404(fun) {
+    fun404 = fun;
+  };
+
+  function go(url) {
+    document.location.hash = url;
+    window.scrollTo(0,0);
+  };
+
+  function toRegex(path) {
+    return (path.constructor == String)
+      ? new RegExp("^"+path.replace(PATH_MATCHER, PATH_REPLACER)+"$")
+      : path;
+  };
+
+  function route(verb, path, cb) {
+    routes[verb].push({
+      path     : toRegex(path),
+      callback : cb
+    });
+  };
+
+  function urlChanged(maintainScroll) {
+    history.push(window.location.hash.slice(1));
+    trigger("GET", window.location.hash.slice(1));
+    if (maintainScroll !== true) {
+      //window.scrollTo(0,0);
+    }
+  };
+
+  function formSubmitted(e) {
+
+    e.preventDefault();
+    var action = e.target.getAttribute("action");
+
+    if (action[0] === "#") {
+      trigger("POST", action.slice(1), e, serialize(e.target));
+    }
+  }
+
+  function trigger(verb, url, ctx, data) {
+    if (preRouterFun) {
+      if (!preRouterFun(verb, url, ctx)) {
+        return;
+      }
+    }
+    var match = matchPath(verb, url);
+    if (match) {
+      var args = match.match.slice(1);
+      if (verb === "POST") {
+        args.unshift(data);
+        args.unshift(ctx);
+      }
+      match.details.callback.apply(this, args);
+    } else {
+      if (fun404) {
+        fun404(verb, url);
+      }
+    }
+  };
+
+  function matchesCurrent(needle) {
+    return window.location.hash.slice(1).match(toRegex(needle));
+  };
+
+  function matchPath(verb, path) {
+    var i, tmp, arr = routes[verb];
+    for (i = 0; i < arr.length; i += 1) {
+      tmp = path.match(arr[i].path);
+      if (tmp) {
+        return {"match":tmp, "details":arr[i]};
+      }
+    }
+    return false;
+  };
+
+  function serialize(obj) {
+    var o = {};
+    var a = $(obj).serializeArray();
+    $.each(a, function() {
+      if (o[this.name]) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+  return {
+    go      : go,
+    back    : back,
+    get     : get,
+    post    : post,
+    init    : init,
+    matchesCurrent : matchesCurrent,
+    pre     : preRouter,
+    refresh : refresh,
+    error404 : error404
+  };
+
+});
+
+var Utils = {};
+
+Utils.isMobile = function() {
+  return navigator.userAgent.toLowerCase()
+    .match(/(android|iphone|ipod|ipad)/) !== null;
+};
+
+
+function linkUp(body, person_prefix, tag_prefix) {
+
+  //body = Mustache.escape(body);
+  person_prefix = person_prefix || "#!/mentions/";
+  tag_prefix = tag_prefix || "#!/tags/";
+
+  var tmp = body.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi,function(a) {
+    return '<a target="_blank" href="'+a+'">'+a+'</a>';
+  });
+
+  function transformText(str) {
+    return str.replace(/\@([\w\-]+)/g, function(user,name) {
+      return '<a href="'+person_prefix+encodeURIComponent(name)+'">'+user+'</a>';
+    }).replace(/\#([\w\-\.]+)/g,function(word,tag) {
+      return '<a href="'+tag_prefix+encodeURIComponent(tag)+'">'+word+'</a>';
+    });
+  };
+
+  function replaceTags(dom) {
+    var i, tmp;
+    for (i = 0; i < dom.childNodes.length; i++) {
+      tmp = (dom.childNodes[i].nodeType === 3 &&
+             $(dom.childNodes[i]).parents("a").length === 0)
+        ? $("<span>"+transformText(dom.childNodes[i].textContent)+"</span>")[0]
+        : replaceTags(dom.childNodes[i]);
+
+      dom.replaceChild(tmp, dom.childNodes[i]);
+    }
+    return dom;
+  };
+
+  var div = document.createElement("div");
+  div.innerHTML = tmp;
+  return replaceTags(div).innerHTML;
+
+  //return tmp;
+};
+
+var dateFormat = function () {
+    var    token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+		timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+		timezoneClip = /[^-+\dA-Z]/g,
+		pad = function (val, len) {
+			val = String(val);
+			len = len || 2;
+			while (val.length < len) val = "0" + val;
+			return val;
+		};
+
+	// Regexes and supporting functions are cached through closure
+	return function (date, mask, utc) {
+		var dF = dateFormat;
+
+		// You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+		if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+			mask = date;
+			date = undefined;
+		}
+
+		// Passing date through Date applies Date.parse, if necessary
+		date = date ? new Date(date) : new Date;
+		if (isNaN(date)) throw SyntaxError("invalid date");
+
+		mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+		// Allow setting the utc argument via the mask
+		if (mask.slice(0, 4) == "UTC:") {
+			mask = mask.slice(4);
+			utc = true;
+		}
+
+		var	_ = utc ? "getUTC" : "get",
+			d = date[_ + "Date"](),
+			D = date[_ + "Day"](),
+			m = date[_ + "Month"](),
+			y = date[_ + "FullYear"](),
+			H = date[_ + "Hours"](),
+			M = date[_ + "Minutes"](),
+			s = date[_ + "Seconds"](),
+			L = date[_ + "Milliseconds"](),
+			o = utc ? 0 : date.getTimezoneOffset(),
+			flags = {
+				d:    d,
+				dd:   pad(d),
+				ddd:  dF.i18n.dayNames[D],
+				dddd: dF.i18n.dayNames[D + 7],
+				m:    m + 1,
+				mm:   pad(m + 1),
+				mmm:  dF.i18n.monthNames[m],
+				mmmm: dF.i18n.monthNames[m + 12],
+				yy:   String(y).slice(2),
+				yyyy: y,
+				h:    H % 12 || 12,
+				hh:   pad(H % 12 || 12),
+				H:    H,
+				HH:   pad(H),
+				M:    M,
+				MM:   pad(M),
+				s:    s,
+				ss:   pad(s),
+				l:    pad(L, 3),
+				L:    pad(L > 99 ? Math.round(L / 10) : L),
+				t:    H < 12 ? "a"  : "p",
+				tt:   H < 12 ? "am" : "pm",
+				T:    H < 12 ? "A"  : "P",
+				TT:   H < 12 ? "AM" : "PM",
+				Z:    utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+				o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+				S:    ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+			};
+
+		return mask.replace(token, function ($0) {
+			return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+		});
+	};
+}();
+
+// Some common format strings
+dateFormat.masks = {
+	"default":      "ddd mmm dd yyyy HH:MM:ss",
+	shortDate:      "m/d/yy",
+	mediumDate:     "mmm d, yyyy",
+	longDate:       "mmmm d, yyyy",
+	fullDate:       "dddd, mmmm d, yyyy",
+	shortTime:      "h:MM TT",
+	mediumTime:     "h:MM:ss TT",
+	longTime:       "h:MM:ss TT Z",
+	isoDate:        "yyyy-mm-dd",
+	isoTime:        "HH:MM:ss",
+	isoDateTime:    "yyyy-mm-dd'T'HH:MM:ss",
+	isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+};
+
+// Internationalization strings
+dateFormat.i18n = {
+	dayNames: [
+		"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+	],
+	monthNames: [
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+		"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+	]
+};
+
+// For convenience...
+Date.prototype.format = function (mask, utc) {
+	return dateFormat(this, mask, utc);
+};
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(date){
+	var diff = (((new Date()).getTime() - date.getTime()) / 1000),
+		day_diff = Math.floor(diff / 86400);
+
+	if ( isNaN(day_diff) || day_diff < 0 )
+		return;
+
+    return day_diff == 0 && (
+		diff < 60 && "just now" ||
+			diff < 120 && "1 min ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " min ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff < 7 && day_diff + " days ago" ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
+    day_diff < 365 && Math.ceil( day_diff / 31) + " months ago";
+};
