54510e6b15b292079c1b84dccff96b12ff58bf4f
Dale Harvey <daleharvey@arandomurl.com>
2011-06-27 14:58:25 +0100

    Removed data size key from ui (attributes removed in new couchdb

--- _attachments/js/mobile.futon.js
+++ _attachments/js/mobile.futon.js
@@ -144,8 +144,7 @@ var MobileFuton = (function () {
                  , db: database
                  , doc_count: info.doc_count
                  , update_seq: info.update_seq
-                 , disk_size: Utils.formatSize(info.disk_size)
-                 , data_size: Utils.formatSize(info.data_size) };
+                 , disk_size: Utils.formatSize(info.disk_size) };
 
       renderer.render('database_tpl', data);
     });
